83490730186fbbb66b0cc2b303bd736ccb4d0053
Antreas Kasiotis <kasiotisandreas@gmail.com>
2021-12-06 18:42:50 +0000

    did some analysis for the question response datasets

--- src/questions-eda.R
+++ src/questions-eda.R
@@ -1,6 +1,18 @@
 #load projTemp library
 library('ProjectTemplate')
-#set my working dir to my projTemp folder
 load.project()
 
-barplot(quiz.data.1$success_percentage, axisnames = TRUE, names.arg = quiz.data.1$quiz) ######### use this later in your analysis
+par(mfrow=c(1,7))
+#checking the success percentage of each quiz for all runs
+barplot(quiz.data.1$success_percentage, axisnames = TRUE, names.arg = quiz.data.1$quiz, col = rainbow(7))
+barplot(quiz.data.2$success_percentage, axisnames = TRUE, names.arg = quiz.data.2$quiz, col = rainbow(7))
+barplot(quiz.data.3$success_percentage, axisnames = TRUE, names.arg = quiz.data.3$quiz, col = rainbow(7))
+barplot(quiz.data.4$success_percentage, axisnames = TRUE, names.arg = quiz.data.4$quiz, col = rainbow(7))
+barplot(quiz.data.5$success_percentage, axisnames = TRUE, names.arg = quiz.data.5$quiz, col = rainbow(7))
+barplot(quiz.data.6$success_percentage, axisnames = TRUE, names.arg = quiz.data.6$quiz, col = rainbow(7))
+barplot(quiz.data.7$success_percentage, axisnames = TRUE, names.arg = quiz.data.7$quiz, col = rainbow(7))
+#the success percentage seems to be approximately the same between the quizzes in all runs 
+
+#checking the correlation of the number of questions and the percentage of success
+pairs(quiz.data.1[,4:5])
+
