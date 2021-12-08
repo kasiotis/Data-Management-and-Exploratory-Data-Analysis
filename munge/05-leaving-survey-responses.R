36f39dcce3af04da5f2cbfc8702b4c61b9a0da36
Antreas Kasiotis <kasiotisandreas@gmail.com>
2021-12-08 17:25:11 +0000

    moved my dataset merging and combining code for the leaving step and reason analysis, from src to munge

--- munge/05-leaving-survey-responses.R
+++ munge/05-leaving-survey-responses.R
@@ -1,6 +1,7 @@
 library(stringi)
-
+#######################################################################################################################################
 ##################################################### Cleaning the Data set ###########################################################
+#######################################################################################################################################
 
 
 #changing the step column so that it represents the step by multiplying the the week by 100 and adding the step number
@@ -26,31 +27,83 @@ clean.leaving.survey.dataset = function(leaving.survey.dataset){
 
 
 
+#######################################################################################################################################
 ################################################## Pre-processing the Data set ########################################################
+#######################################################################################################################################
+
+
+
+##################################################  Working on the Leaving step  ########################################################
+
+
+########################## Extracting the frequency for each leaving step ###############################
 
 
 #function that takes in a leaving survey dataset and returns data about the leaving steps
-get.leaving.step.data = function(leaving.survey.dataset){
+get.leaving.step.data = function(leaving.survey.dataset, myrun){
   
   #finding the frequency of the each step at which students left. Also calling the clean function implemented above, to clean my data
   #before using it to construct new data.
   leaving.step = as.data.frame(table(clean.leaving.survey.dataset(leaving.survey.dataset)$last_completed_step))
 
   #returning a data frame with all of the steps that were found to be leaving steps along with the number of people that left at each step.
-  return(data.frame(leaving.steps = leaving.step$Var1,
-                    frequency = leaving.step$Freq))
+  return(data.frame(
+    run = rep(myrun, length(leaving.step$Var1)),
+    leaving.steps = leaving.step$Var1,
+    frequency = leaving.step$Freq))
 }
 
 #calling the function that creates the leaving step data, for all the runs that has such datasets
-leaving.step.data.4 = get.leaving.step.data(cyber.security.4_leaving.survey.responses)
-leaving.step.data.5 = get.leaving.step.data(cyber.security.5_leaving.survey.responses)
-leaving.step.data.6 = get.leaving.step.data(cyber.security.6_leaving.survey.responses)
-leaving.step.data.7 = get.leaving.step.data(cyber.security.7_leaving.survey.responses)
+leaving.step.data.4 = get.leaving.step.data(cyber.security.4_leaving.survey.responses,4)
+leaving.step.data.5 = get.leaving.step.data(cyber.security.5_leaving.survey.responses,5)
+leaving.step.data.6 = get.leaving.step.data(cyber.security.6_leaving.survey.responses,6)
+leaving.step.data.7 = get.leaving.step.data(cyber.security.7_leaving.survey.responses,7)
 
+
+######################### producing a merged set of all the runs ##############################
+
+
+get.leaving.steps.data.merged = function(allleavingsteps) {
+  
+  #getting the unique names of the age ranges from my new set
+  step.names = unique(allleavingsteps$leaving.steps)
+  
+  #initializing vectors that will hold the frequencies of each leaving step
+  all.step.frequencies = 1:length(step.names)
+  
+  #extracting the frequencies of each leaving step from all runs
+  for (i in 1:length(step.names)) {
+    all.step.frequencies[i] = sum(allleavingsteps$frequency[allleavingsteps$leaving.steps == step.names[i]])
+  }
+  
+  #Creating a new data frame with my extracted data
+  leaving.steps.data.merged = data.frame(
+    leaving.step = as.numeric(as.character(step.names)),
+    frequency = all.step.frequencies
+  )
+  #reordering the dataset by the leaving step so that it can be plotted nicely and returning it
+  return(leaving.steps.data.merged[order(leaving.steps.data.merged$leaving.step),])
+}
+
+#merging all the leaving reason data sets that I have created for all runs
+leaving.steps.data.allruns = merge(merge(merge(
+  leaving.step.data.4,
+  leaving.step.data.5, all=TRUE),
+  leaving.step.data.6, all=TRUE),
+  leaving.step.data.7, all=TRUE)
+
+#calling the function I just created to get the merged version of runs 4-7
+leaving.steps.data.merged = get.leaving.steps.data.merged(leaving.steps.data.allruns)
+
+
+#################################################  Working on the Leaving Reason  #######################################################
+
+
+########################## Extracting the frequency for each leaving reason ###############################
 
 
 #function that takes in a leaving survey dataset and returns data about the leaving reasons
-get.leaving.reason.data = function(leaving.survey.dataset){
+get.leaving.reason.data = function(leaving.survey.dataset, myrun){
   
   #cleaning the dataset in order for the leaving reason to be rid of all non=UTF-8 characters
   leaving.survey.dataset = clean.leaving.survey.dataset(leaving.survey.dataset)
@@ -62,12 +115,48 @@ get.leaving.reason.data = function(leaving.survey.dataset){
                                         leaving.survey.dataset$leaving_reason!="I prefer not to say"]))
 
   #returning a data frame with all of the reason that were given for leaving along with the number of people that left because of that reason.
-  return(data.frame(leaving.reasons = leaving.reason$Var1,
-                    frequency = leaving.reason$Freq))
+  return(data.frame(
+    run = rep(myrun, length(leaving.reason$Var1)),
+    leaving.reasons = leaving.reason$Var1,
+    frequency = leaving.reason$Freq))
 }
 
 #calling the function that creates the leaving reason data, for all the runs that has such datasets
-leaving.reason.data.4 = get.leaving.reason.data(cyber.security.4_leaving.survey.responses)
-leaving.reason.data.5 = get.leaving.reason.data(cyber.security.5_leaving.survey.responses)
-leaving.reason.data.6 = get.leaving.reason.data(cyber.security.6_leaving.survey.responses)
-leaving.reason.data.7 = get.leaving.reason.data(cyber.security.7_leaving.survey.responses)
+leaving.reason.data.4 = get.leaving.reason.data(cyber.security.4_leaving.survey.responses,4)
+leaving.reason.data.5 = get.leaving.reason.data(cyber.security.5_leaving.survey.responses,5)
+leaving.reason.data.6 = get.leaving.reason.data(cyber.security.6_leaving.survey.responses,6)
+leaving.reason.data.7 = get.leaving.reason.data(cyber.security.7_leaving.survey.responses,7)
+
+
+######################### producing a merged set of all the runs ##############################
+
+
+get.leaving.reason.data.merged = function(allreasons){
+  
+  
+  #getting the unique names of the age ranges from my new set
+  reason.names = unique(allreasons$leaving.reasons)
+  
+  #initializing vectors that will hold the frequencies of each leaving reason
+  all.reason.frequencies = 1:length(reason.names)
+  
+  #extracting the total frequencies of each leaving reason from all runs
+  for (i in 1:length(reason.names)) {
+    all.reason.frequencies[i] = sum(allreasons$frequency[allreasons$leaving.reasons == reason.names[i]])
+  }
+  
+  #Creating a new data frame with my extracted data
+   data.frame(
+    leaving.reason = reason.names,
+    frequency = all.reason.frequencies
+  )
+}
+
+#merging all the leaving reason data sets that I have created for runs 4-7
+leaving.reason.data.allruns = merge(merge(merge(
+  leaving.reason.data.4,
+  leaving.reason.data.5, all=TRUE),
+  leaving.reason.data.6, all=TRUE),
+  leaving.reason.data.7, all=TRUE)
+
+leaving.reason.data.merged = get.leaving.reason.data.merged(leaving.reason.data.allruns)
--- src/leaving-survey-responses-eda.R
+++ src/leaving-survey-responses-eda.R
@@ -2,74 +2,22 @@ library(ProjectTemplate)
 load.project()
 
 ##############################################################################################################################################
-########################################################## Analysis on the leaving reason ####################################################
-##############################################################################################################################################
-
-
-
-#merging all the leaving reason data sets that I have created for all runs
-allreasons = merge(merge(merge(
-  leaving.reason.data.4,
-  leaving.reason.data.5, all=TRUE),
-  leaving.reason.data.6, all=TRUE),
-  leaving.reason.data.7, all=TRUE)
- 
-
-#getting the unique names of the age ranges from my new set
-reason.names = unique(allreasons$leaving.reasons)
-
-#initializing vectors that will hold the numbers of enrollments and graduations of each country
-all.reason.frequencies = 1:length(reason.names)
-
-#extracting the enrollments and graduations of each country from all runs
-for (i in 1:length(reason.names)) {
-  all.reason.frequencies[i] = mean(allreasons$frequency[allreasons$leaving.reasons == reason.names[i]])
-}
-
-#recalculating the the graduate percentage of each country by merging the findings of all runs
-leaving.reason.data.merged = data.frame(
-  leaving.reason = reason.names,
-  frequency = all.reason.frequencies
-)
-
-#plotting the merged leaving reasons
-barplot(leaving.reason.data.merged$frequency, axisnames = TRUE, names.arg = leaving.reason.data.merged$leaving.reason, col = 1:6)
-
-
-##############################################################################################################################################
 ########################################################## Analysis on the leaving step ######################################################
 ##############################################################################################################################################
 
-
-#merging all the leaving reason data sets that I have created for all runs
-allleavingsteps = merge(merge(merge(
-  leaving.step.data.4,
-  leaving.step.data.5, all=TRUE),
-  leaving.step.data.6, all=TRUE),
-  leaving.step.data.7, all=TRUE)
-
-
-#getting the unique names of the age ranges from my new set
-step.names = unique(allleavingsteps$leaving.steps)
-
-#initializing vectors that will hold the numbers of enrollments and graduations of each country
-all.step.frequencies = 1:length(step.names)
-
-#extracting the enrollments and graduations of each country from all runs
-for (i in 1:length(step.names)) {
-  all.step.frequencies[i] = mean(allleavingsteps$frequency[allleavingsteps$leaving.steps == step.names[i]])
-}
-
-#recalculating the the graduate percentage of each country by merging the findings of all runs
-leaving.steps.data.merged = data.frame(
-  leaving.step = as.numeric(as.character(step.names)),
-  frequency = all.step.frequencies
-)
-#reordering the dataset by the leaving step so that it can be plotted nicely
-leaving.steps.data.merged =  leaving.steps.data.merged[order(leaving.steps.data.merged$leaving.step),]
 
 barplot(leaving.steps.data.merged$frequency, axisnames = TRUE, names.arg = leaving.steps.data.merged$leaving.step, col = 1:42, axis.lty = 1,
         xlab = "Leaving Step", ylab = "Frequency", ylim = c(0,max(leaving.steps.data.merged$frequency)*1.1), cex.axis=0.7, cex.names=0.7,
         main = "Average number of people that have left at each step (all runs)")
 
+
+
+##############################################################################################################################################
+########################################################## Analysis on the leaving reason ####################################################
+##############################################################################################################################################
+
+
+#plotting the merged leaving reasons
+barplot(leaving.reason.data.merged$frequency, axisnames = TRUE, names.arg = leaving.reason.data.merged$leaving.reason, col = 1:6,
+        ylim = c(0,max(leaving.reason.data.merged$frequency)*1.2))
 
